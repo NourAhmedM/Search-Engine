c263a1c71e114f0d62de4a431c93f101b95024c2
sara <sara.yasser.kamal@gmail.com>
2020-05-23 12:15:54 +0200

    tf/idf

--- Relevance.java
+++ Relevance.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,9 +10,11 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Relevance {
 
-	Set<String> words;
-	public Relevance (Set<String> words) {
+	String[] words;
+	int size;
+	public Relevance (String[] words) {
 		this.words = words;
+		this.size = words.length;
 	}
 	
 	public static wordValue tempCreatData() {
@@ -41,8 +45,41 @@ public class Relevance {
 		return wordVal;
 	}
 	
+	public Map<Integer, String> ranker(Map<String, wordValue> wordsDictionary) {
+		wordValue wordVal;
+		float idf;
+		Map<Integer, List<Float> > tdfDictionary;
+		for(int i = 0; i < this.size; i++)  // loop on every word in the query
+		{
+			wordVal = wordsDictionary.get(words[i]);
+			idf = wordVal.idf;
+			tdfDictionary = wordVal.tdfDictionary;
+		}
+		return null;
+	}
+	
+	public float rank(float TF, float IDF) { // fore now just tf/idf
+		if (TF > 0.5)
+			return 0;
+		float IDF_log = (float) Math.log(IDF);
+		return IDF_log*TF;
+	}
+	
 	 public static void main(String[] args) {
-		 wordValue w = tempCreatData();
-		 w.print();
+		wordValue w = tempCreatData();
+		w.print();
+		 
+		Set<String> hash_Set = new HashSet<String>(); 
+		hash_Set.add("Geeks"); 
+		hash_Set.add("For"); 
+		hash_Set.add("Geeks"); 
+		hash_Set.add("Example"); 
+		hash_Set.add("Set"); 
+		hash_Set.toArray();
+		String[] queryWords = hash_Set.toArray(new String[0]);
+		System.out.println(queryWords.length); 
+		
+		Map<String, wordValue> wordsDictionary = new HashMap<String, wordValue>();;
+		wordsDictionary.put("Geeks", w);
 	 }
 }
