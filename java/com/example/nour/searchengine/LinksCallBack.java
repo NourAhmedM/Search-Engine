cc16c1bba1bc932f3d97f5bbe2ef69b095443c33
Nour <nouraly28898@gmail.com>
2020-06-08 23:40:40 +0200

    edit

--- java/com/example/nour/searchengine/LinksCallBack.java
+++
@@ -1,9 +0,0 @@
-package com.example.nour.searchengine;
-
-import java.util.ArrayList;
-
-
-
-public abstract class LinksCallBack {
-    public void LinksResponse(ArrayList<SearchItem> links,int numberOfLinks){}
-}
--- java/com/example/nour/searchengine/LinksNumCallBack.java
+++
@@ -1,7 +0,0 @@
-package com.example.nour.searchengine;
-
-import java.util.ArrayList;
-
-public class LinksNumCallBack {
-    public void LinksResponse(Integer linksNum){}
-}
